1c60b9c8a675ce8c7b9ab37ab663978294658879
Tamsin Huggins <tamsin.hns@gmail.com>
2023-10-13 15:00:56 +0100

    able to access a script element containing the key information of a single chord page (Ed Sheeran Perfect) using its url

---
+++ key_scraping/key_author_title.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+URL = 'https://tabs.ultimate-guitar.com/tab/ed-sheeran/perfect-chords-1956589'
+response = requests.get(URL)
+
+page = response.text
+
+pagesoup = BeautifulSoup(page, "html.parser")
+
+script_tag = pagesoup.find_all('script', type='application/ld+json')
+
+
+
+print(script_tag)
+
+# # Access the JSON data inside the script tag
+# if script_tag:
+#     json_data = script_tag.string
+#     # You now have the JSON data as a string
+
+#     # You can parse it into a Python dictionary using a JSON library like json
+#     import json
+#     data = json.loads(json_data)
+
+#     # Now you can access the data in the JSON object
+#     print(data['name'])
+#     # print(data['composer']['name'])
+#     print(data['datePublished'])
+#     print(data['dateModified'])
+#     print(data['musicalKey'])
+# else:
+#     print("Script tag not found.")
