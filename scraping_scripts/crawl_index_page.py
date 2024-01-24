732e305590d8c04f6e6569bf022b84f09a19d472
Tamsin Huggins <tamsin.hns@gmail.com>
2024-01-24 22:01:26 +0000

    4955 songs scraped

--- scraping_scripts/crawl_index_page.py
+++ scraping_scripts/crawl_index_page.py
@@ -6,10 +6,13 @@ from get_song_chords import get_song_chords
 JAN24_COOKIES_BUTTON_CLASS = "css-197f1ny"
 JAN24_CHORD_PAGE_LINK_CLASSNAME= "aPPf7 HT3w5 lBssT" # this may change, use the inspector to check
 test_chord_page= "https://tabs.ultimate-guitar.com/tab/ed-sheeran/perfect-chords-1956589"
-counter = 498 # this will label the song files, 1 to 497 are already completed.
+counter = 4906 # this will label the song files, 1 to 497 are already completed.
 
+ # 2 to 12 completed
+# 13 to 100 completed
+# 101 to 200 next
 
-for num in range(13, 100): # 2 to 12 completed
+for num in range(1, 2):
     #try to generate a list of page URLs, if failed continue to the next page
     try:
         page_URLs = generate_list_of_chord_page_links(f"https://www.ultimate-guitar.com/explore?page={num}&type[]=Chords", JAN24_COOKIES_BUTTON_CLASS, JAN24_CHORD_PAGE_LINK_CLASSNAME)
