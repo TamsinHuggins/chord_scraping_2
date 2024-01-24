053abd36e09b80b31eb1693e065654ad4b24e0d0
Tamsin Huggins <tamsin.hns@gmail.com>
2024-01-24 18:58:21 +0000

    now able to scrape info and chords-lyrics for 40 songs in one go

--- scraping_scripts/get_song_chords.py
+++ scraping_scripts/get_song_chords.py
@@ -4,25 +4,35 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
+import os
 
-test_URL= "https://tabs.ultimate-guitar.com/tab/ed-sheeran/perfect-chords-1956589"
+# test_URL= "https://tabs.ultimate-guitar.com/tab/jeff-buckley/hallelujah-chords-198052"
 
-def get_lyrics_chords(URL):
 #  use beautifulsoup to make a soup object of the whole page
-    response = requests.get(test_URL)
+def get_song_chords(URL, num):
+    """ given a URL of a song page, writes the chords and lyrics of the song to a text.
+    Parameters:
+    URL (str): URL of the song page
+    num (int): number to label the song file"""
+
+    response = requests.get(URL)
     page = response.text
     page_soup = BeautifulSoup(page, "html.parser")
     #find all the divs with the class js-store
     info = page_soup.find_all('div', { 'class' : 'js-store'})[0]
-    print(page_soup)
     info = str(info)
+
     # use regex to find any text after the first [ch]
-    begin = re.findall(r'\[ch\](.*)', info)[0]
-    snipped = begin.split("&quot;")[0]
-    return snipped
+    chords_lyrics = re.findall(r'\[ch\](.*)', info)[0]
 
+    # cut all the junk after the end of the chords lyrics section
+    snipped_chords_lyrics = chords_lyrics.split("&quot;")[0]
 
-#  call get lyrics on the URL, then save to a file called buckleyInfo.txt
-lyrics = get_lyrics_chords(test_URL)
-with open("sheeranChordsLyrics.txt", "w") as f:
-    f.write(lyrics)
+    # write the file into the folder called scraped
+    current_dir = os.getcwd()
+    if not current_dir.endswith('scraped'):
+        # If not, change to the 'scraped' directory
+        os.chdir('scraped')
+
+    with open( f"song_{num}_chords_lyrics.txt", "w") as f:
+        f.write(str(snipped_chords_lyrics))
